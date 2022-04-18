eb83cdc1d2707135b42fd65414cf220253e602b7
Sovandara UOM <Sovandara.Uom@jtrustroyal.com>
2022-04-18 10:04:56 +0700

    Initialize project

---
+++ test/cdk-lambda-event-schedule.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as CdkLambdaEventSchedule from '../lib/cdk-lambda-event-schedule-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/cdk-lambda-event-schedule-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new CdkLambdaEventSchedule.CdkLambdaEventScheduleStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
