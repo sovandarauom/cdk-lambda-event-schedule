727359bf0d1446853586d224d9de9c98fef6b5f8
Sovandara UOM <Sovandara.Uom@jtrustroyal.com>
2022-04-25 09:44:32 +0700

    list ec2

--- lib/cdk-lambda-event-schedule-stack.ts
+++ lib/cdk-lambda-event-schedule-stack.ts
@@ -10,18 +10,32 @@ export class CdkLambdaEventScheduleStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    const newLambda = new Function(this, 'newLambda',{
+    const cronjob = new Function(this, 'cronjob',{
       runtime: Runtime.NODEJS_14_X,
       code: Code.fromAsset(path.join(__dirname, 'handlers')),
       handler: 'cron.handler',
       memorySize: 128,
       logRetention: RetentionDays.ONE_DAY,
     });
+    const lambdaTargetCron = new LambdaFunction(cronjob);
+    
+    const listEc2 = new Function(this, 'listEc2',{
+      runtime: Runtime.NODEJS_14_X,
+      code: Code.fromAsset(path.join(__dirname, 'handlers')),
+      handler: 'list-image.handler',
+      memorySize: 128,
+      logRetention: RetentionDays.ONE_DAY,
+    });
+    const lambdaTargetListEc2 = new LambdaFunction(listEc2);
 
-    const lambdaTarget = new LambdaFunction(newLambda);
-    const eventRule = new Rule(this, 'scheduleRule', {
-      schedule: Schedule.expression('cron(0/5 * * * ? *)'),
-      targets: [lambdaTarget],
+    const eventRuleCron = new Rule(this, 'eventRuleCron', {
+      schedule: Schedule.expression('cron(0/30 * * * ? *)'),
+      targets: [lambdaTargetCron],
+    });
+
+    const eventRuleListEc2 = new Rule(this, 'eventRuleListEc2', {
+      schedule: Schedule.expression('cron(0/10 * * * ? *)'),
+      targets: [lambdaTargetListEc2],
     });
   }
 }
